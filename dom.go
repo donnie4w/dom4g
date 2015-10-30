5be8c313440ee57159a0720706981260cedbca8f
yanzhh <yanzhh@jingoal.com>
2015-10-30 13:51:31 +0800

    support blank before element.

--- dom.go
+++ dom.go
@@ -84,10 +84,10 @@ func LoadByStream(r io.Reader) (current *Element, err error) {
 				current = current.parent.(*Element)
 			}
 		case xml.CharData:
-			current.Value = string([]byte(token))
-		case xml.Comment:
-		case xml.Directive:
-		case xml.ProcInst:
+			if current != nil {
+				current.Value = string([]byte(token))
+			}
+		case xml.Comment, xml.Directive, xml.ProcInst:
 		default:
 			panic("parse xml fail!")
 		}
